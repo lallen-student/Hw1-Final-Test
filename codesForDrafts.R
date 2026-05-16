e17c52f82164bc1de49ecab80c611168b45ddc5c
lallen-student <lallendelice@umass.edu>
2026-05-15 22:17:37 -0400

    edits

--- codesForDrafts.R
+++ codesForDrafts.R
@@ -21,6 +21,7 @@ head(mydata)
 str(mydata)
 
 # deliverable 1 ----------------------------------------------------------
+
 library(ggplot2)
 
 base= ggplot(data=mydata) 
@@ -51,23 +52,3 @@ del3Draft
 # save del3Draft ----------------------------------------------------------
 saveRDS(del3Draft, file = "del3Draft.rds")
 
-
-# deliverable 4  ----------------------------------------------------------
-
-library(sf)
-county_map=sf::read_sf("WA_County_Boundaries.geojson")
-head(county_map)
-head(mydata)
-
-# merge data into map ----------------------------------------------------------
-mydataCounty=aggregate(data=mydata,Free.Lunch~County,FUN = mean)
-myMapLunch=merge(county_map,mydataCounty,by.x='JURISDIC_2',"County")
-
-# prepare plot
-
-base=ggplot(myMapLunch)
-del4Draft=base + geom_sf(aes(fill=Free.Lunch))
-del4Draft
-
-# save del4Draft ----------------------------------------------------------
-saveRDS(del4Draft, file = "del4Draft.rds")
